36ebcd7bfa42f3ba9c1d42a23ec6706e72d29b0c
arsh98000 <arsh98000@gmail.com>
2023-01-11 15:38:39 -0700

    Add AuthenticationService, .env file, api.js and update app.js to have an endpoint.

--- back-end/app.js
+++ back-end/app.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const port = process.env.PORT || 5000;
+
+
+app.post('/register', (request, response) => {
+    response.send({
+        message: `User ${request.body.username} was registered!`,
+    });
+    console.log('User was created.');
+});
+
+
+app.listen(port);
+console.log('App is listenting on http://localhost:5000');
